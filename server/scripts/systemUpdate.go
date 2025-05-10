e6444cf13f5521c93c8c0a783e5f49cf99e8b8ad
codeforge11 <codeforge11@gmail.com>
2025-05-10 14:55:29 +0200

    refactor: optimize OS detection for update script

--- server/scripts/systemUpdate.go
+++ server/scripts/systemUpdate.go
@@ -1,53 +1,85 @@
 package scripts
 
 import (
+	"bufio"
+	"log"
 	"net/http"
-
+	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Update(c *gin.Context) {
-	err := exec.Command("/bin/sh", "-c", `
-	update_ubuntu_debian_raspbian() {
-		sudo apt update && sudo apt upgrade -y
-	}
+	var detectedID string
 
-	update_fedora_RedHat() {
-		sudo dnf update && sudo dnf upgrade --refresh -y
-	}
+	file, err := os.Open("/etc/os-release")
 
-	update_arch(){
-		sudo pacman -Syu -y
-	}
-
-	if [ -f /etc/os-release ]; then
-		. /etc/os-release
-		case "$ID" in
-			ubuntu|debian|raspbian)
-				update_ubuntu_debian_raspbian
-				;;
-			fedora|ultramarine|rhel)
-				update_fedora_RedHat
-				;;
-			arch|manjaro)
-				update_arch
-				;;
-			*)
-				echo "Unsupported distribution $ID"
-				;;
-		esac
-	else
-		echo "ERROR"
-	fi
-	`).Run()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+		log.Fatal("Error opening /etc/os-release:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to open file"})
 		LogError(err)
 		return
 	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "ID=") {
+			detectedID = strings.TrimPrefix(line, "ID=")
+			detectedID = strings.Trim(detectedID, `"`)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error reading file:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read file"})
+		LogError(err)
+		return
+
+	}
+
+	switch detectedID {
+	case "arch":
+		cmd := exec.Command("/bin/sh", "-c", `sudo pacman -Syu -y`)
+
+		err = cmd.Run()
+		if err != nil {
+			log.Fatal("Error running update command:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+			LogError(err)
+			return
+		}
+
+	case "debian", "ubuntu":
+		cmd := exec.Command("/bin/sh", "-c", `sudo apt update -y && sudo apt upgrade -y`)
+
+		err = cmd.Run()
+		if err != nil {
+			log.Fatal("Error running update command:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+			LogError(err)
+			return
+		}
+
+	case "fedora":
+		cmd := exec.Command("/bin/sh", "-c", `sudo dnf update -y`)
+
+		err = cmd.Run()
+		if err != nil {
+			log.Fatal("Error running update command:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+			LogError(err)
+			return
+		}
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "unsupported distribution"})
+		LogMessage("Unsupported distribution")
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"status": "update script executed"})
 	LogMessage("Update script executed")
-
 }
