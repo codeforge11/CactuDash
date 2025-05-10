9c056ab2f0c6820a9a250b6b4cdfef603a4d8ec5
codeforge11 <codeforge11@gmail.com>
2025-05-10 19:34:56 +0200

    refactor: introduce the correct system detections

--- server/scripts/systemUpdate.go
+++ server/scripts/systemUpdate.go
@@ -6,80 +6,100 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Update(c *gin.Context) {
+
 	var detectedID string
+	var detectedIDLike string
+	var supportStatus bool
+	var osName string
 
-	file, err := os.Open("/etc/os-release")
-
+	file, err := os.Open("/etc/os-release") //for detect distro name
 	if err != nil {
-		log.Fatal("Error opening /etc/os-release:", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to open file"})
+		log.Println("Error opening /etc/os-release:", err)
 		LogError(err)
-		return
-	}
-	defer file.Close()
+	} else {
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.HasPrefix(line, "ID=") {
+				detectedID = strings.TrimPrefix(line, "ID=")
+				detectedID = strings.Trim(detectedID, `"`)
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.HasPrefix(line, "ID=") {
-			detectedID = strings.TrimPrefix(line, "ID=")
-			detectedID = strings.Trim(detectedID, `"`)
+			}
+			if strings.HasPrefix(line, "ID_LIKE=") {
+				detectedIDLike = strings.TrimPrefix(line, "ID_LIKE=")
+				detectedIDLike = strings.Trim(detectedIDLike, `"`)
+
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			log.Println("Error reading file:", err)
+			LogError(err)
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error reading file:", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read file"})
-		LogError(err)
-		return
-
+	if runtime.GOOS == "linux" {
+		osName = detectedID
+	} else {
+		osName = runtime.GOOS
 	}
 
-	switch detectedID {
-	case "arch":
-		cmd := exec.Command("/bin/sh", "-c", `sudo pacman -Syu -y`)
+	if osName == detectedID {
+		supportStatus = true
+	} else if osName == detectedIDLike {
+		supportStatus = true
+	} else {
+		supportStatus = false
+	}
 
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal("Error running update command:", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
-			LogError(err)
+	if supportStatus {
+		switch osName {
+		case "arch":
+			cmd := exec.Command("/bin/sh", "-c", `sudo pacman -Syu -y`)
+
+			err = cmd.Run()
+			if err != nil {
+				log.Fatal("Error running update command:", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+				LogError(err)
+				return
+			}
+
+		case "debian", "ubuntu":
+			cmd := exec.Command("/bin/sh", "-c", `sudo apt update -y && sudo apt upgrade -y`)
+
+			err = cmd.Run()
+			if err != nil {
+				log.Fatal("Error running update command:", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+				LogError(err)
+				return
+			}
+
+		case "fedora":
+			cmd := exec.Command("/bin/sh", "-c", `sudo dnf update -y`)
+
+			err = cmd.Run()
+			if err != nil {
+				log.Fatal("Error running update command:", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
+				LogError(err)
+				return
+			}
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "unsupported distribution"})
+			LogMessage("Unsupported distribution")
 			return
 		}
 
-	case "debian", "ubuntu":
-		cmd := exec.Command("/bin/sh", "-c", `sudo apt update -y && sudo apt upgrade -y`)
-
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal("Error running update command:", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
-			LogError(err)
-			return
-		}
-
-	case "fedora":
-		cmd := exec.Command("/bin/sh", "-c", `sudo dnf update -y`)
-
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal("Error running update command:", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to run update script"})
-			LogError(err)
-			return
-		}
-	default:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "unsupported distribution"})
-		LogMessage("Unsupported distribution")
-		return
+		c.JSON(http.StatusOK, gin.H{"status": "update script executed"})
+		LogMessage("Update script executed")
 	}
-
-	c.JSON(http.StatusOK, gin.H{"status": "update script executed"})
-	LogMessage("Update script executed")
 }
