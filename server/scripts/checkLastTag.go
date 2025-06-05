b280df1d2d8fabaa13a621041e4dfac09276dfe6
codeforge11 <codeforge11@gmail.com>
2025-06-05 21:39:07 +0200

    feat: add endpoint to fetch last Git tag from GitHub

--- server/scripts/checkLastTag.go
+++ server/scripts/checkLastTag.go
@@ -2,8 +2,10 @@ package scripts
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,37 +14,49 @@ type Tag struct {
 	Name string `json:"name"`
 }
 
-func GetLastGitTag(c *gin.Context) {
-	url := "https://api.github.com/repos/codeforge11/CactuDash/tags" //url of project
+var (
+	LastGitTag     string
+	lastGitTagLock sync.RWMutex
+)
+
+func GetLastGitTagName() (string, error) {
+	url := "https://api.github.com/repos/codeforge11/CactuDash/tags"
 
 	resp, err := http.Get(url)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch tags"})
-		return
+		return "", err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode == http.StatusOK {
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response body"})
-			return
-		}
-
-		var tags []Tag
-		err = json.Unmarshal(body, &tags)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to unmarshal JSON"})
-			return
-		}
-
-		if len(tags) > 0 {
-			c.JSON(http.StatusOK, gin.H{"last_tag": tags[0].Name})
-			return
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{"error": "No tags found"})
-			return
-		}
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("failed to fetch tags")
 	}
-	c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch tags"})
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var tags []Tag
+	err = json.Unmarshal(body, &tags)
+	if err != nil {
+		return "", err
+	}
+
+	if len(tags) > 0 {
+		lastGitTagLock.Lock()
+		LastGitTag = tags[0].Name
+		lastGitTagLock.Unlock()
+		return tags[0].Name, nil
+	}
+	return "", errors.New("no tags found")
+}
+
+func GetLastGitTag(c *gin.Context) {
+	tag, err := GetLastGitTagName()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"last_tag": tag})
 }
